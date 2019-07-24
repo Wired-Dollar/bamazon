6b92a53db413689e4c088f33dfd03c5756c86ef5
Wired-Dollar <wired.1.dollar@gmail.com>
2019-07-24 18:04:31 -0500

    database created

--- damazon.sql
+++ damazon.sql
@@ -2,4 +2,45 @@ DROP DATABASE IF EXISTS bamazon_db;
 CREATE DATABASE bamazon_db;
 USE bamazon_db;
 
-CREATE TABLE products
+CREATE TABLE products (
+    item_id VARCHAR (30),
+    name VARCHAR(30) NOT NULL,
+    department_name VARCHAR (30),
+    price INTEGER (10),
+    stock_quantity INTEGER (5),
+);
+INSERT INTO products (item_id, name, department_name, price, stock_quantity)
+VALUES (3428," Apple Watch", electronics, 399, 6);
+
+INSERT INTO products (item_id, name, department_name, price, stock_quantity)
+VALUES (3429, "iPhone X", electronics, 899, 2);
+
+INSERT INTO products (item_id, name, department_name, price, stock_quantity)
+VALUES (3430, "iPad Pro", electronics, 699, 5);
+
+INSERT INTO products (item_id, name, department_name, price, stock_quantity)
+VALUES (3431, "iPad", electronics, 299, 15);
+
+INSERT INTO products (item_id, name, department_name, price, stock_quantity)
+VALUES (3432, "iPad mini", electronics, 399, 5);
+
+INSERT INTO products (item_id, name, department_name, price, stock_quantity)
+VALUES (3433, "MacBook", electronics, 1399, 6);
+
+INSERT INTO products (item_id, name, department_name, price, stock_quantity)
+VALUES (3434, "Galaxy Note 9", electronics, 999, 5);
+
+INSERT INTO products (item_id, name, department_name, price, stock_quantity)
+VALUES (3435, "Galaxy Watch", electronics, 399, 5);
+
+INSERT INTO products (item_id, name, department_name, price, stock_quantity)
+VALUES (3439, "Galaxy S10", electronics, 799, 5);
+
+INSERT INTO products (item_id, name, department_name, price, stock_quantity)
+VALUES (3438, "Galaxy S10+", electronics, 899, 10);
+
+INSERT INTO products (item_id, name, department_name, price, stock_quantity)
+VALUES (3437, "Galaxy Tab A", electronics, 299, 10);
+
+INSERT INTO products (item_id, name, department_name, price, stock_quantity)
+VALUES (3436, "Galaxy Tab S4", electronics, 499, 5);
